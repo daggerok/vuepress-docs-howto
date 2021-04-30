ed3d1fac5bcbc02a69af0711e799db1b8f064fdf
Maksim Kostromin <daggerok@gmail.com>
2021-04-30 19:30:46 +0300

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,19 +1,25 @@
-const baseHref = !!process.env.BASE_HREF ? process.env.BASE_HREF : '/';
+const base = process.env.BASE || '/';
+
 module.exports = {
     title: 'VuePress howto',
+    /**
+     * Extra tags to be injected to the page HTML `<head>`
+     *
+     * ref：https://v1.vuepress.vuejs.org/config/#head
+     */
     head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }]
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'icon', href: '/favicon.ico' }],
     ],
-    base: baseHref,
+    base,
     themeConfig: {
         repo: 'daggerok/vuepress-docs-how-too',
-        // lastUpdated: 'Last Updated',
     },
     markdown: {
-        lineNumbers: true,
         extendMarkdown: md => {
-            // awesome md enhancer! partials includes!
-            md.use(require('markdown-it-vuepress-code-snippet-enhanced'))
-        }
+            md.use(require('markdown-it-vuepress-code-snippet-enhanced'));
+        },
     },
 };
